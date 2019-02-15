063eefbbb118b62c3f458fb1d432f35fe4822a27
chunlinli <li000007@umn.edu>
2019-02-14 20:46:56 -0600

    a toy example

---
+++ main.R
@@ -0,0 +1,81 @@
+#
+# R version of cmleDAG
+# 
+# @reference: Li, Shen, Pan "Likelihood ratio tests for a large directed acyclic graph"
+# @author: Chunlin Li (li000007@umn.edu), Ziyue Zhu (zhux0502@umn.edu)
+# @version: 0.18.12.19
+# 
+
+
+library(mvtnorm)
+source("clrdag.R")
+
+#
+# Example 1: random graph
+#
+
+set.seed(2018)
+t1 = proc.time()
+
+p=100
+n=200
+# random graph: randomly generate adjacency matrix A, A lower triangular
+sparsity = 0.01
+A = matrix(rbinom(p*p,1,sparsity)*sign(runif(p*p,min=-1,max=1)),p,p)
+A[upper.tri(A, diag = T)] = 0
+Sigma = solve(diag(p) - A)
+Sigma = Sigma%*%t(Sigma)
+# X ~ Normal(0,Sigma)
+X = rmvnorm(n,mean=rep(0,p), sigma=Sigma, method = "chol") 
+out = cmleDAG(X=X,tau=0.35,mu=1,rho=1.2)
+Eout = ifelse(out$A!=0,1,0)
+E = ifelse(A!=0,1,0)
+
+sum(abs(E - Eout))
+sum(E - Eout > 0)
+
+proc.time() - t1
+
+
+
+
+
+
+set.seed(2018)
+
+t1 = proc.time()
+p=200
+n=200
+# hub graph: randomly generate adjacency matrix A, A lower triangular
+A = matrix(0,p,p)
+A[,1] = sign(runif(p,min=-1,max=1))
+A[1,1] = 0
+Sigma = solve(diag(p) - A)
+Sigma = Sigma%*%t(Sigma)
+# X ~ Normal(0,Sigma)
+X = rmvnorm(n,mean=rep(0,p), sigma=Sigma, method = "chol") 
+out = cmleDAG(X=X,tau=0.35,mu=1,rho=1.2)
+Eout = ifelse(out$A!=0,1,0)
+E = ifelse(A!=0,1,0)
+sum(abs(E - Eout))
+sum(E - Eout > 0)
+proc.time() - t1
+
+
+
+
+
+
+
+
+
+
+
+XTX = crossprod(X)
+
+for(j in 1:p)
+{
+    XTX[,j] = XTX[,j]/XTX[j,j]
+}
+
+XTX[,1]
