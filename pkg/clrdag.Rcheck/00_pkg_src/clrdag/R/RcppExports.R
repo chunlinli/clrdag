84a26e49c20dec2d005f1c2422d311a9377e1293
li000007 <li000007@umn.edu>
2019-06-09 10:24:27 -0500

    CRAN v0.6.0

--- pkg/clrdag.Rcheck/00_pkg_src/clrdag/R/RcppExports.R
+++
@@ -1,4 +0,0 @@
-DC_ADMM <- function(X, A, Lambda, D, tau, mu, rho, tol_abs, tol_rel, dc_max_iter, admm_max_iter, auto_init, trace_obj) {
-    .Call(`_clrdag_DC_ADMM`, X, A, Lambda, D, tau, mu, rho, tol_abs, tol_rel, dc_max_iter, admm_max_iter, auto_init, trace_obj)
-}
-
